661eaeabe0c5615d512e54fca69b60676cc80112
admin <dangwc@gz-mstc.com>
2017-04-05 10:22:41 +0800

    修改网络请求框架

--- dwc_MVVM/NetworkManager/BaseNetManager.m
+++ dwc_MVVM/NetworkManager/BaseNetManager.m
@@ -9,8 +9,6 @@
 #import "BaseNetManager.h"
 #import <AFNetworking/AFNetworking.h>
 #import "WCHeader.h"
-//#import "NSObject+WCTool.h"
-
 
 @implementation BaseNetManager
 
@@ -30,6 +28,7 @@ static AFHTTPSessionManager *manager = nil;
         [manager.requestSerializer setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
         manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html",@"text/plain",@"application/json", nil];
         
+        //验证用户的accesstoken
 //        NSString *urlStr = [NSString stringWithFormat:@"Bearer %@", [LinkUserContext context].auth.accessToken];
 //        [manager.requestSerializer setValue:urlStr forHTTPHeaderField:@"Authorization"];
         
@@ -38,30 +37,34 @@ static AFHTTPSessionManager *manager = nil;
 }
 
 + (id)GET:(NSString *)path parameters:(NSDictionary *)params completionHandler:(void(^)(id responseObj, NSError *error))complete{
-    //打印网络请求， DDLog  与  NSLog 功能一样
+    
     NSLog(@"Request Path: %@, Params:%@", path, params);
-    return [[self sharedAFManager] GET:path parameters:params success:^void(NSURLSessionDataTask * task, id responseObject) {
+    
+    return [[self sharedAFManager] GET:path parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         //成功后的处理
         complete(responseObject, nil);
-    } failure:^void(NSURLSessionDataTask * task, NSError * error) {
-        complete(nil, error);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+         complete(nil, error);
     }];
+    
 }
 
 + (id)POST:(NSString *)path parameters:(NSDictionary *)params completionHandler:(void(^)(id responseObj, NSError *error))complete{
-    return [[self sharedAFManager] POST:path parameters:params success:^void(NSURLSessionDataTask * task, id responseObject) {
+    
+    
+    return [[self sharedAFManager] POST:path parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         complete(responseObject, nil);
-    } failure:^void(NSURLSessionDataTask * task, NSError * error) {
-       // [self handleError:error];
-        complete(nil, error);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+         complete(nil, error);
     }];
+    
 }
 
 
 + (void)post:(NSString *)URLString parameters:(id)parameters success:(void (^)(id))success failure:(void (^)(NSError *))failure
 {
     
-    [[self sharedAFManager] POST:URLString parameters:parameters success:^(NSURLSessionDataTask * _Nonnull task, id  _Nonnull responseObject) {
+    [[self sharedAFManager] POST:URLString parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         if (success) {
             success(responseObject);
             NSLog(@"post");
@@ -77,17 +80,18 @@ static AFHTTPSessionManager *manager = nil;
 
 + (void)get:(NSString *)URLString parameters:(id)parameters success:(void (^)(id))success failure:(void (^)(NSError *))failure
 {
-
-    [[self sharedAFManager] GET:URLString parameters:parameters success:^(NSURLSessionDataTask * _Nonnull task, id  _Nonnull responseObject) {
-                if (success) {
-                    success(responseObject);
-                    NSLog(@"get");
-                }
+    
+    [[self sharedAFManager] GET:URLString parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        //成功后的处理
+        if (success) {
+            success(responseObject);
+            NSLog(@"get");
+        }
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                if (failure) {
-                    failure(error);
-                    NSLog(@"get");
-                }
+        if (failure) {
+            failure(error);
+            NSLog(@"get");
+        }
     }];
 }
 
